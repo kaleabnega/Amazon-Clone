4b7764ee51f77953eb4d4a567ebd9e2a6fedd07d
Kaleab <kaleabalebachew3@gmail.com>
2025-08-25 17:46:52 +0400

    Changed port number

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
